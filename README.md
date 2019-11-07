5d7a40efd3efd70a59f0dc565aff2788bfb1b3fb
Eric Trombly <etrombly@yahoo.com>
2019-11-07 11:52:38 -0600

    initial release

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ## About
 
-This template demonstrates the minimum code and tooling necessary for a frontend web app with simple deployable artifacts consisting of one HTML file, one JavaScript file, and one WebAssembly file, using [`Yew`](https://github.com/yewstack/yew), [`wasm-bindgen`](https://github.com/rustwasm/wasm-bindgen), and [`wasm-pack`](https://github.com/rustwasm/wasm-pack).
+This is a minimal example of the gcode-rs crate with web assembly.
 
-Note: [`yew-wasm-pack-template`](https://github.com/yewstack/yew-wasm-pack-template) is the full-featured counterpart to this template, integrating many common web technologies.
+![Screenshot](/screenshot.png?raw=true "Screenshot")
 
 ## Usage
 
-### 1) Install `Rust` and `wasm-pack`
+### 1) Install `Rust`, `wasm-pack`, and "rollup"
 
 Follow the instructions at https://www.rust-lang.org/tools/install and follow the `installation` link at [`wasm-pack`](https://github.com/rustwasm/wasm-pack).
 
@@ -18,13 +18,9 @@ Enter `wasm-pack build --target web` from your project's root directory.
 
 Enter `rollup ./main.js --format iife --file ./pkg/bundle.js` from your project's root directory.
 
-Note: Until `wasm-pack` [RFC #6](https://github.com/rustwasm/rfcs/blob/master/text/006-local-js-dependencies.md) is implemented there is no available option to [generate a single amalgamated JavaScript file](https://github.com/rustwasm/wasm-pack/issues/699).  In the interim a bundler, such as [`Rollup`](https://rollupjs.org/guide/en/#quick-start), must be used.
-
 ### 4) [optional] Test Run
 
-Run a webserver from your project's root directory, such as with `python -m SimpleHTTPServer 8080`, and load http://localhost:8080/ in a browser to run the app.
-
-Note: It's expected behavior for the browser console to display an error similar to "WebAssembly.instantiateStreaming failed. Assuming this is because your server does not serve wasm with application/wasm MIME type."  Your production webserver should be configured to associate WebAssembly files with the `application/wasm` MIME type.
+Run a webserver from your project's root directory, such as with `python3 -m http.server`, and load http://localhost:8000/ in a browser to run the app.
 
 ### 5) Deploy
 
--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,51 @@
 use yew::prelude::*;
+use stdweb::{
+    traits::*,
+    unstable::TryInto,
+    console,
+    web::{
+        document,
+        html_element::CanvasElement,
+        CanvasRenderingContext2d,
+    },
+};
+use gcode::Mnemonic;
 
-pub struct App {}
+#[derive(Debug)]
+struct State {
+  input: String,
+}
 
-pub enum Msg {}
+pub struct App {
+    state: State,
+}
+
+pub enum Msg {
+    ProcessGcode,
+    UpdateInput(String),
+}
 
 impl Component for App {
     type Message = Msg;
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        App {}
+        let state = State{input: "".to_string()};
+        App {state}
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::ProcessGcode => {
+                {
+                    self.state.draw_map(&self.state.input);
+                }
+                self.state.input = "".to_string();
+            }
+            Msg::UpdateInput(val) => {
+                self.state.input = val;
+            }
+        }
         true
     }
 }
@@ -20,7 +53,73 @@ impl Component for App {
 impl Renderable<App> for App {
     fn view(&self) -> Html<Self> {
         html! {
-            <p>{ "Hello world!" }</p>
+            <div class="row">
+                { self.view_input() }
+                { self.view_canvas() }
+            </div>
         }
+    }
+}
+
+impl App {
+    fn view_input(&self) -> Html<App> {
+        html! {
+            <div class="column left">
+                <textarea id="gcode_input" rows="10" cols="30" value=&self.state.input oninput=|e| Msg::UpdateInput(e.value)/>
+                <br/>
+                <button onclick=|_| Msg::ProcessGcode>{"Process Gcode"}</button>
+            </div>
+        }
+    }
+
+    fn view_canvas(&self) -> Html<App> {
+        html! {
+            <div class="column middle">
+                <canvas id="gcode_canvas" width="640" height="480"></canvas>
+            </div>
+        }
+    }
+}
+
+impl State {
+    fn draw_map(&self, gcode: &str) {
+        let canvas: CanvasElement = document()
+            .query_selector("#gcode_canvas")
+            .unwrap()
+            .expect("Didn't find the map canvas.")
+            .try_into() // Element -> CanvasElement
+            .unwrap(); // cannot be other than a canvas
+        let context: CanvasRenderingContext2d = canvas.get_context().unwrap();
+
+        context.clear_rect(0., 0., canvas.width() as f64, canvas.height() as f64);
+        context.set_line_width(1.0);
+        context.begin_path();
+        context.set_fill_style_color("black");
+        // fix for blurry lines
+        context.translate(0.5, 0.5);
+        context.move_to(0.0, 0.0);
+
+        let lines = gcode::parse(gcode);
+        for line in lines {
+            for code in line.gcodes() {
+                match code.mnemonic() {
+                    Mnemonic::General => {
+                        match code.major_number() {
+                            0 | 1 => {
+                                if let Some(x) = code.value_for('x') {
+                                    if let Some(y) = code.value_for('y') {
+                                        //console!(log, format!("line to {} {}", x, y));
+                                        context.line_to(x.into(),y.into());
+                                    }
+                                }
+                            },
+                            _ => {},
+                        }
+                    },
+                    _ => {},
+                }
+            }
+        }
+        context.stroke();
     }
 }
