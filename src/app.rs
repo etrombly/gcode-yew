c3dfc4436b1aad8c37415be5fc315dfebcd69a82
Eric Trombly <etrombly@yahoo.com>
2019-11-07 16:46:50 -0600

    improve rendering for G0

--- src/app.rs
+++ src/app.rs
@@ -1,19 +1,24 @@
-use yew::prelude::*;
+use gcode::{GCode, Mnemonic};
 use stdweb::{
+    console,
     traits::*,
     unstable::TryInto,
-    console,
-    web::{
-        document,
-        html_element::CanvasElement,
-        CanvasRenderingContext2d,
-    },
+    web::{document, html_element::CanvasElement, CanvasRenderingContext2d},
 };
-use gcode::Mnemonic;
+use yew::prelude::*;
 
 #[derive(Debug)]
+struct Location {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+#[derive(Debug)]
 struct State {
-  input: String,
+    input: String,
+    location: Location,
+    display_z: f64,
+    draw_moves: bool,
 }
 
 pub struct App {
@@ -21,8 +26,11 @@ pub struct App {
 }
 
 pub enum Msg {
+    Clear,
+    DrawMove,
     ProcessGcode,
     UpdateInput(String),
+    UpdateZ(String),
 }
 
 impl Component for App {
@@ -30,20 +38,29 @@ impl Component for App {
     type Properties = ();
 
     fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        let state = State{input: "".to_string()};
-        App {state}
+        let state = State {
+            input: "".to_string(),
+            location: Location {
+                x: 0.,
+                y: 0.,
+                z: 0.,
+            },
+            display_z: 0.,
+            draw_moves: true,
+        };
+        App { state }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::ProcessGcode => {
-                {
-                    self.state.draw_map(&self.state.input);
-                }
-                self.state.input = "".to_string();
+                self.state.draw_map(&self.state.input.clone());
             }
-            Msg::UpdateInput(val) => {
-                self.state.input = val;
+            Msg::DrawMove => self.state.draw_moves = !self.state.draw_moves,
+            Msg::UpdateInput(val) => self.state.input = val,
+            Msg::UpdateZ(val) => self.state.display_z = val.parse::<f64>().unwrap(),
+            Msg::Clear => {
+                self.state.input = "".to_string();
             }
         }
         true
@@ -67,7 +84,12 @@ impl App {
             <div class="column left">
                 <textarea id="gcode_input" rows="10" cols="30" value=&self.state.input oninput=|e| Msg::UpdateInput(e.value)/>
                 <br/>
-                <button onclick=|_| Msg::ProcessGcode>{"Process Gcode"}</button>
+                <button onclick=|_| Msg::Clear>{"Clear"}</button>
+                <button onclick=|_| Msg::ProcessGcode>{"Process"}</button>
+                <div>
+                    <input type="checkbox" id="draw_move" checked=self.state.draw_moves onclick=|_| Msg::DrawMove/>
+                    {"Draw Moves"}
+                </div>
             </div>
         }
     }
@@ -76,13 +98,16 @@ impl App {
         html! {
             <div class="column middle">
                 <canvas id="gcode_canvas" width="640" height="480"></canvas>
+                <br/>
+                {"Z layer: "}<input type="range" id="display_z_slider" value=&self.state.display_z oninput=|e| Msg::UpdateZ(e.value)/>
+                {self.state.display_z}
             </div>
         }
     }
 }
 
 impl State {
-    fn draw_map(&self, gcode: &str) {
+    fn draw_map(&mut self, gcode: &str) {
         let canvas: CanvasElement = document()
             .query_selector("#gcode_canvas")
             .unwrap()
@@ -92,9 +117,11 @@ impl State {
         let context: CanvasRenderingContext2d = canvas.get_context().unwrap();
 
         context.clear_rect(0., 0., canvas.width() as f64, canvas.height() as f64);
-        context.set_line_width(1.0);
-        context.begin_path();
-        context.set_fill_style_color("black");
+        self.location = Location {
+                x: 0.,
+                y: 0.,
+                z: 0.,
+            };     
         // fix for blurry lines
         context.translate(0.5, 0.5);
         context.move_to(0.0, 0.0);
@@ -103,22 +130,59 @@ impl State {
         for line in lines {
             for code in line.gcodes() {
                 match code.mnemonic() {
-                    Mnemonic::General => {
-                        match code.major_number() {
-                            0 | 1 => {
-                                if let Some(x) = code.value_for('x') {
-                                    if let Some(y) = code.value_for('y') {
-                                        //console!(log, format!("line to {} {}", x, y));
-                                        context.line_to(x.into(),y.into());
-                                    }
-                                }
-                            },
-                            _ => {},
+                    Mnemonic::General => match code.major_number() {
+                        0 | 1 => {
+                            self.parse_G0(code, &context);
                         }
+                        _ => {}
                     },
-                    _ => {},
+                    _ => {}
                 }
             }
+        }
+        context.translate(-0.5, -0.5);
+    }
+
+    #[allow(non_snake_case)]
+    fn parse_G0(&mut self, code: &GCode, context: &CanvasRenderingContext2d) {
+        if let Some(z) = code.value_for('z') {
+            self.location.z = z as f64;
+        }
+
+        let mut draw = self.draw_moves;
+        let mut color = "black";
+
+        match code.value_for('e') {
+            Some(e) => {
+                draw = true;
+                if e < 0. {
+                    // color retracts red
+                    color = "red";
+                }
+            },
+            // if moves are drawn color them green
+            None => color = "green",
+        }
+        context.set_line_width(1.0);
+        context.begin_path();
+        context.set_stroke_style_color(color);
+        context.move_to(self.location.x, self.location.y);
+        if let Some(x) = code.value_for('x') {
+            self.location.x = x.into();
+        }
+        if let Some(y) = code.value_for('y') {
+            self.location.y = y.into();
+        }
+
+        // TODO: figure out how wide the z drawing should be (above and below current z)
+        // if the code isn't on the display layer don't draw
+        if self.location.z - self.display_z > 0.1 || self.location.z - self.display_z < -0.1 {
+            console!(log, "skipping code, not on display z layer {}", self.location.z - self.display_z);
+            draw = false;
+        }
+        
+        if draw {
+            context.line_to(self.location.x, self.location.y);
         }
         context.stroke();
     }
