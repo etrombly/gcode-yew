b35059d2676efddb9eeaca7a0e5790ac4b866372
Eric Trombly <etrombly@yahoo.com>
2019-11-08 16:29:20 -0600

    switch to bulma.css for themeing and layout

--- src/app.rs
+++ src/app.rs
@@ -57,11 +57,14 @@ impl Component for App {
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
             Msg::ProcessGcode => {
-                self.state.draw_map(&self.state.input.clone());
+                self.state.draw_map();
             }
             Msg::DrawMove => self.state.draw_moves = !self.state.draw_moves,
             Msg::UpdateInput(val) => self.state.input = val,
-            Msg::UpdateZ(val) => self.state.display_z = val.parse::<f64>().unwrap(),
+            Msg::UpdateZ(val) => {
+                self.state.display_z = val.parse::<f64>().unwrap();
+                self.state.draw_map();
+            }
             Msg::Clear => {
                 self.state.input = "".to_string();
             }
@@ -73,7 +76,7 @@ impl Component for App {
 impl Renderable<App> for App {
     fn view(&self) -> Html<Self> {
         html! {
-            <div class="row">
+            <div class="columns is-centered">
                 { self.view_input() }
                 { self.view_canvas() }
             </div>
@@ -84,52 +87,72 @@ impl Renderable<App> for App {
 impl App {
     fn view_input(&self) -> Html<App> {
         html! {
-            <div class="column left">
-                <textarea id="gcode_input" rows="10" cols="30" value=&self.state.input oninput=|e| Msg::UpdateInput(e.value)/>
-                <br/>
-                <button onclick=|_| Msg::Clear>{"Clear"}</button>
-                <button onclick=|_| Msg::ProcessGcode>{"Process"}</button>
-                <div>
-                    <input type="checkbox" id="draw_move" checked=self.state.draw_moves onclick=|_| Msg::DrawMove/>
-                    {"Draw Moves"}
+            <div class="column is-one-quarter">
+                <p class="message-header">
+                    {"GCode"}
+                </p>
+                <textarea id="gcode_input" class="textarea" rows="20" placeholder="Enter GCode here" value=&self.state.input oninput=|e| Msg::UpdateInput(e.value)/>
+                <div class="buttons is-grouped is-right">
+                    <p class="control"><button class="button is-dark" onclick=|_| Msg::Clear>{"Clear"}</button></p>
+                    <p class="control"><button class="button is-dark" onclick=|_| Msg::ProcessGcode>{"Process"}</button></p>
                 </div>
+                <p class="message is-dark">
+                    <p class="message-header">
+                        {"Options"}
+                    </p>
+                    <div class="message-body">
+                        <label class="checkbox">
+                            <input type="checkbox" id="draw_move" class="checkbox" checked=self.state.draw_moves onclick=|_| Msg::DrawMove/>
+                            {"Draw Moves"}
+                        </label>
+                    </div>
+                </p>
             </div>
         }
     }
 
     fn view_canvas(&self) -> Html<App> {
         html! {
-            <div class="column middle">
-                <canvas id="gcode_canvas" width="640" height="480"></canvas>
-                <br/>
-                {"Z layer: "}<input type="range" id="display_z_slider" value=&self.state.display_z oninput=|e| Msg::UpdateZ(e.value)/>
-                {self.state.display_z}
+            <div class="column is-two-thirds">
+                <p class="message-header">
+                    {"Output"}
+                </p>
+                <canvas id="gcode_canvas" class="box" style="width:100%;height:80%;"></canvas>
+                <div class="field is-grouped is-grouped-centered">
+                    <p class="control"><label class="label">{"Z layer "}</label></p>
+                    <input type="range" id="display_z_slider" class="control is-expanded" value=&self.state.display_z oninput=|e| Msg::UpdateZ(e.value)/>
+                    <input type="text" class="tag is-dark" size="4" value=&self.state.display_z oninput=|e| Msg::UpdateZ(e.value)/>
+                </div>
             </div>
         }
     }
 }
 
 impl State {
-    fn draw_map(&mut self, gcode: &str) {
+    fn draw_map(&mut self) {
         let canvas: CanvasElement = document()
             .query_selector("#gcode_canvas")
             .unwrap()
             .expect("Didn't find the map canvas.")
             .try_into() // Element -> CanvasElement
             .unwrap(); // cannot be other than a canvas
+                       // size the canvas to match the actual width and height, get's rid of blurriness
+        canvas.set_width(canvas.offset_width() as u32);
+        canvas.set_height(canvas.offset_height() as u32);
         let context: CanvasRenderingContext2d = canvas.get_context().unwrap();
 
         context.clear_rect(0., 0., canvas.width() as f64, canvas.height() as f64);
         self.location = Location {
-                x: 0.,
-                y: 0.,
-                z: 0.,
-            };     
-        // fix for blurry lines
+            x: 0.,
+            y: 0.,
+            z: 0.,
+        };
+        // another fix for blurry lines
         context.translate(0.5, 0.5);
         context.move_to(0.0, 0.0);
 
-        let lines = gcode::parse(gcode);
+        let gcode = self.input.clone();
+        let lines = gcode::parse(&gcode);
         for line in lines {
             for code in line.gcodes() {
                 match code.mnemonic() {
@@ -166,7 +189,7 @@ impl State {
                     // color retracts red
                     color = "red";
                 }
-            },
+            }
             // if moves are drawn color them green
             None => color = "green",
         }
@@ -184,7 +207,11 @@ impl State {
         // TODO: figure out how wide the z drawing should be (above and below current z)
         // if the code isn't on the display layer don't draw
         if self.location.z - self.display_z > 0.1 || self.location.z - self.display_z < -0.1 {
-            console!(log, "skipping code, not on display z layer {}", self.location.z - self.display_z);
+            console!(
+                log,
+                "skipping code, not on display z layer {}",
+                self.location.z - self.display_z
+            );
             draw = false;
         }
 
@@ -212,7 +239,7 @@ impl State {
                     // color retracts red
                     color = "red";
                 }
-            },
+            }
             // if moves are drawn color them green
             None => color = "green",
         }
@@ -241,7 +268,9 @@ impl State {
             ((x1 - self.location.x).powf(2.) + (y1 - self.location.y).powf(2.)).sqrt()
         };
         if draw {
-            context.arc_to(x1, y1, self.location.x, self.location.y, r).expect("failed to draw arc");
+            context
+                .arc_to(x1, y1, self.location.x, self.location.y, r)
+                .expect("failed to draw arc");
         }
         context.stroke();
     }
